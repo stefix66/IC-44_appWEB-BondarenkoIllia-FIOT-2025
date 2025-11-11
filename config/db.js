9b5d2b479b287e19b8a63f949bb0e6333b08cf5c
Ilya <ilabondarenko781@gmail.com>
2025-11-11 12:51:28 +0200

    feat: initialize backend server and connect PostgreSQL (Neon)
    Initialized Node.js project and configured Express backend.
    Added environment variables and Sequelize configuration for Neon PostgreSQL database.
    Verified successful database connection and server launch on port 5000.

---
+++ config/db.js
@@ -0,0 +1,29 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+{
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+}
+
+);
+
+export const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Database connected successfully");
+  } catch (error) {
+    console.error("❌ Database connection failed:", error);
+  }
+};
---
+++ models/index.js
@@ -0,0 +1,10 @@
+import { sequelize } from "../config/db.js";
+import { DataTypes } from "sequelize";
+
+export const User = sequelize.define("User", {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+});
+
+export default { User };
